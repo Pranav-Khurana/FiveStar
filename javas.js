280f6d846b869e58ae0faef3905dbdcbd9a6f34f
Pranav Khurana <apranavkhurana18@gmail.com>
2019-06-28 15:44:37 +0530

    added all files and folders

---
+++ javas.js
@@ -0,0 +1,15 @@
+function main()
+{
+alert('Welcome to Five Stars.......\nThere might be error, sorry for inconvenience');
+$('.mem').hide();
+$('.eve').hide();
+for(var i=0; i<1000; i++)
+{
+$('img').fadeOut(10000);
+$('img').fadeIn(10000);
+}
+$('.button').on('click', function () { $(this).toggleClass('togler'); $(this).next().slideToggle(1000);  } );
+$('#pics').on('click', function () { window.open("C:/Users/pranav/Desktop/programing/FiveStar/five_stars_gallery.html") } );
+
+}
+$(document).ready(main);
